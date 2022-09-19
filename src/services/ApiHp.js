6f289db4dad9a7dce2c5b71a823d20b5247c5fdb
Marianela Landoni Herrera <mlandonih@hotmail.com>
2022-09-19 18:17:03 +0200

    Change api url

--- src/services/ApiHp.js
+++ src/services/ApiHp.js
@@ -1,7 +1,7 @@
 import { v4 as uuidv4 } from 'uuid';
 
 function getDataApi() {
-  return fetch('http://hp-api.herokuapp.com/api/characters')
+  return fetch('https://hp-api.herokuapp.com/api/characters')
     .then((response) => response.json())
     .then((data) => {
       const dataClean = data.map((character) => {
